2217b5ada05dffc0e4075a328d069cc889856d11
jeka1488 <mogudi88>
2020-08-21 14:24:57 +0300

    Add View Author, Book and Account //Need Add Methods in AuthorAndBookController

---
+++ Library/Controllers/AuthorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Library.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class AuthorController : Controller
+    {
+        
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return null;
+        }
+        public IActionResult Create()
+        {
+            return null;
+        }
+        [HttpPost]
+        public IActionResult Create(AuthorVM authorVm)
+        {
+            return null;
+        }
+        
+        public IActionResult Edit([FromRoute]int id)
+        {
+            return null;
+        }
+       
+        [HttpPost]
+        public IActionResult Edit(AuthorVM authorVm)
+        {
+            return null;
+        }
+        
+        public IActionResult Delete([FromRoute]int id)
+        {
+            return null;
+        }
+    }
+}
---
+++ Library/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Library.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class BookController : Controller
+    {
+       
+        // GET
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return null;
+        }
+
+        public IActionResult Create()
+        {
+            return null;
+
+        }
+        
+        [HttpPost]
+        public IActionResult Create(CreateBookVM createBookVm)
+        {
+            return null;
+        }
+        
+        public IActionResult Edit([FromRoute]int id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditBookVM editBookVm)
+        {
+            return null;
+        }
+
+        public IActionResult Delete([FromRoute]int id)
+        {
+            return null;
+        }
+    }
+}

627a4f7afb4faa8b86181b1cdf0dcdce262814be
jeka1488 <mogudi88>
2020-08-21 15:46:09 +0300

    Final

--- Library/Program.cs
+++ Library/Program.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Controllers.Helpers;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +13,16 @@ namespace Library
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var serviceScope = host.Services.CreateScope();
+            var services = serviceScope.ServiceProvider;
+
+            var init = services.GetRequiredService<InitHelper>();
+            await init.Init();
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
